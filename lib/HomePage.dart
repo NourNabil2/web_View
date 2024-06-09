7110e0c4524ce46e6fe3df2da16eaea9223b5c59
NourNabil2 <nour60g@gmail.com>
2024-06-09 13:19:36 +0300

    last

---
+++ lib/HomePage.dart
@@ -0,0 +1,60 @@
+import 'dart:io';
+
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:webview_flutter/webview_flutter.dart';
+
+class WebPage extends StatefulWidget {
+  const WebPage({super.key});
+
+  @override
+  State<WebPage> createState() => _WebPageState();
+}
+bool isloading = false;
+class _WebPageState extends State<WebPage> {
+  final webController = WebViewController()
+    ..setJavaScriptMode(JavaScriptMode.unrestricted)
+    ..loadRequest(Uri.parse('https://ramezps.com/'));
+  @override
+  void initState() {
+    webController.setNavigationDelegate(
+        NavigationDelegate(
+      onWebResourceError: (error) {
+        if (error.errorType == WebResourceErrorType.hostLookup) {
+          setState(() {
+            isloading = true;
+          });
+        }
+        else
+          {
+            setState(() {
+              isloading = false;
+            });
+          }
+      },
+    ));
+    super.initState();
+  }
+  @override
+  Widget build(BuildContext context) {
+    webController.setNavigationDelegate(NavigationDelegate(
+      onWebResourceError: (error) {
+        if (error.errorType == WebResourceErrorType.hostLookup) {
+          setState(() {
+            isloading = true;
+          });
+        }
+        else
+        {
+          setState(() {
+            isloading = false;
+          });
+        }
+      },
+    ));
+    return Scaffold(
+      body: isloading ? const CircularProgressIndicator(color: Colors.green,) : WebViewWidget(controller: webController),
+    );
+
+  }
+}
